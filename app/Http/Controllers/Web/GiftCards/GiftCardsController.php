ef18c331c789f86e8b801d84d099a25bdc213091
Dan Pastori <dan@521dimensions.com>
2022-02-21 15:15:25 -0800

    Added gift card controller

---
+++ app/Http/Controllers/Web/GiftCards/GiftCardsController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Web\GiftCards;
+
+use App\Http\Controllers\Controller;
+use Inertia\Inertia;
+use App\Models\Accounts\GiftCard;
+
+class GiftCardsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+        $this->middleware('verified');
+        $this->middleware('can:show,giftCard')->only('show');
+    }
+
+    public function show( GiftCard $giftCard )
+    {
+        
+    }
+}

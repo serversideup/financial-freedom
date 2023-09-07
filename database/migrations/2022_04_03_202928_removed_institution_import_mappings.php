11bbd764fc5be132471100f9dd4b09923ecdd985
Jay Rogers <jay@521dimensions.com>
2023-09-07 15:07:03 -0500

    Commented out migration for now

--- database/migrations/2022_04_03_202928_removed_institution_import_mappings.php
+++ database/migrations/2022_04_03_202928_removed_institution_import_mappings.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('institutions_import_mappings');
+        // Schema::dropIfExists('institutions_import_mappings');
     }
 
     /**

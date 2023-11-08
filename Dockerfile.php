ba028a65ae5dcf7ff75b12ae1494e42fb5d1a48c
Jay Rogers <jay@521dimensions.com>
2023-11-08 21:18:22 +0000

    Revert file

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,11 +1,6 @@
 FROM serversideup/php:beta-8.2.12-unit-bookworm as base
 LABEL maintainer="Server Side Up <@serversideup>"
 
-FROM base as development
-ARG USER_ID
-
-RUN usermod -u $USER_ID www-data
-
 # Copy the application with the correct permissions
 FROM base as deploy
 COPY --chown=9999:9999 . /var/www/html

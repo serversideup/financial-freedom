9ad071c7eaef92a5c04fb923ee795f0ad75c9b84
Jay Rogers <jay@521dimensions.com>
2023-11-09 14:19:23 -0600

    Update Docker build and publish workflow and Dockerfiles

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,11 +1,12 @@
 FROM serversideup/php:beta-8.2.12-unit-bookworm as base
-LABEL maintainer="Server Side Up <@serversideup>"
 
+### Development
 FROM base as development
 ARG USER_ID
 
 RUN usermod -u $USER_ID www-data
 
-# Copy the application with the correct permissions
+### Deploy
 FROM base as deploy
+LABEL maintainer="Server Side Up <@serversideup>"
 COPY --chown=9999:9999 . /var/www/html

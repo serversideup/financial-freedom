c42525638de59f9e05324608a47aaab3589e59fb
Jay Rogers <jay@521dimensions.com>
2023-11-09 16:22:39 -0600

    Create automated workflows with Docker Image (#57)
    * Added workflow file
    
    * Fixed name reference
    
    * Added login action
    
    * Set latest
    
    * Set 0.0.02
    
    * Added chmod
    
    * Use 0.0.3
    
    * Set platforms
    
    * Added registry
    
    * Added version
    
    * Bumped version
    
    * Set to v1
    
    * Update .dockerignore to ignore new files and
    directories
    
    * Add beta Docker image publishing workflow and
    service
    
    * Remove unnecessary "required" field in
    workflow_call input
    
    * Update Dockerfile for beta release
    
    * Fix Dockerfile path in
    publish_docker-images-beta.yml
    
    * Remove unnecessary line in Docker Publish workflow
    and update Site Deployment workflow.
    
    * Add docker build step to GitHub Actions workflow
    
    * Updated spin
    
    * Add USER_ID argument to php build in
    docker-compose.dev.yml
    
    * Add report.xml to .gitignore
    
    * Add Docker and static site deployment workflows
    
    * Add Docker Publish workflow for Edge Image
    
    * Update build configuration for edge images
    
    * Add Node.js service to docker-compose.ci.yml
    
    * Update Docker build and publish workflow and
    Dockerfiles
    
    * Set names
    
    * Update Node Build and PHP Unit workflows
    
    * Add checkout step and restore composer cache in
    service_docker-build-and-publish.yml workflow
    
    * Dont continue on error
    
    * Refactor cache restore and save actions in GitHub
    workflows
    
    * Update cache restore actions in
    service_docker-build-and-publish.yml.
    
    * Refactor caching in GitHub Actions workflows
    
    * Add cache hit check for composer and node_modules
    
    * Updated dependency files
    
    * Fixed keys
    
    * Updated dependencies
    
    * Update user ID in Dockerfile.php
    
    * Updated to v4
    
    * Update branch for push trigger in
    action_app_publish-app-edge.yml

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,11 +1,12 @@
 FROM serversideup/php:beta-8.2.12-unit-bookworm as base
-LABEL maintainer="Server Side Up <@serversideup>"
 
+### Development
 FROM base as development
 ARG USER_ID
 
 RUN usermod -u $USER_ID www-data
 
-# Copy the application with the correct permissions
+### Deploy
 FROM base as deploy
-COPY --chown=9999:9999 . /var/www/html
+LABEL maintainer="Server Side Up <@serversideup>"
+COPY --chown=33:33 . /var/www/html

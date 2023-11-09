a164719ed0b13b59dc9a4c7c0a462b65c7dabaed
Jay Rogers <jay@521dimensions.com>
2023-11-09 15:38:53 -0600

    Update user ID in Dockerfile.php

--- Dockerfile.php
+++ Dockerfile.php
@@ -9,4 +9,4 @@ RUN usermod -u $USER_ID www-data
 ### Deploy
 FROM base as deploy
 LABEL maintainer="Server Side Up <@serversideup>"
-COPY --chown=9999:9999 . /var/www/html
+COPY --chown=33:33 . /var/www/html

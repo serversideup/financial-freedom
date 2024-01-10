f7fba87dcf82bc152a87d0357dcb06fbab31911e
Jay Rogers <jay@521dimensions.com>
2024-01-10 10:39:27 -0600

    Add introduction and information about Financial Freedom

---
+++ docs/components/content/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex items-center justify-center space-x-4">
+      <div class="not-prose flex flex-col items-center justify-center">
+          <img src="https://serversideup.net/wp-content/uploads/2023/08/dan.jpg" alt="Dan Pastori" class="rounded-full w-[150px] h-[150px]">
+          <div class="w-full flex items-center justify-center mt-5 space-x-5">
+              <a href="https://twitter.com/danpastori" target="_blank">
+                  <img src="/images/logos/x-logo.svg" class="w-5"/>
+              </a>
+              <a href="https://github.com/danpastori" target="_blank">
+                  <img src="/images/logos/github-white.svg" class="w-5"/>
+              </a>
+          </div>
+      </div>
+      <div class="not-prose flex flex-col items-center justify-center">
+          <img src="https://serversideup.net/wp-content/uploads/2023/08/jay.jpg" alt="Jay Rogers" class="rounded-full w-[150px] h-[150px]">
+          <div class="w-full flex items-center justify-center mt-5 space-x-5">
+              <a href="https://twitter.com/jaydrogers" target="_blank">
+                  <img src="/images/logos/x-logo.svg" class="w-5"/>
+              </a>
+              <a href="https://github.com/jaydrogers" target="_blank">
+                  <img src="/images/logos/github-white.svg" class="w-5"/>
+              </a>
+          </div>
+      </div>
+  </div>
+</template>

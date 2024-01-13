0a8c8bd3883effd5a5939f86e2f2108a0d6f25b9
Dan Pastori <dan@521dimensions.com>
2024-01-13 10:01:52 -0800

    Added default currency to users

---
+++ database/migrations/2024_01_13_180043_added_default_currency_to_users.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('default_currency')->default('USD')->after('remember_token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};

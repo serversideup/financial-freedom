d90a66464f745811a4757e33504a1e96a7dc7ab9
Dan Pastori <dan@521dimensions.com>
2024-01-19 13:37:32 -0800

    Added institutions controller

---
+++ app/Http/Controllers/InstitutionsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class InstitutionsController extends Controller
+{
+    public function index( Request $request )
+    {
+
+    }
+
+    public function store( Request $request )
+    {
+
+    }
+
+    public function show( Request $request ): Response
+    {
+
+    }
+
+    public function update( Request $request )
+    {
+
+    }
+
+    public function destroy( Request $request ): RedirectResponse
+    {
+        
+    }
+}

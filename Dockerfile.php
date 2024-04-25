b5bcfe90c1fe075c55dbcc867c141218e8bca8ea
Jay Rogers <jay@521dimensions.com>
2024-04-25 08:04:19 -0500

    Update Docker set up to support new beta of serversideup/php v3

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,15 +1,31 @@
+############################################
+# Base Image
+############################################
+
 # Learn more about the Server Side Up PHP Docker Images at:
 # https://serversideup.net/open-source/docker-php/
+FROM serversideup/php:beta-8.3-fpm-nginx as base
 
-FROM serversideup/php:beta-8.3-unit as base
-
+############################################
+# Development Image
+############################################
 FROM base as development
 
-# Fix permission issues in development by setting the "www-data"
-# user to the same user and group that is running docker.
+# We can pass USER_ID and GROUP_ID as build arguments
+# to ensure the www-data user has the same UID and GID
+# as the user running Docker.
 ARG USER_ID
 ARG GROUP_ID
-RUN docker-php-serversideup-set-id www-data ${USER_ID} ${GROUP_ID}
 
+# Switch to root so we can set the user ID and group ID
+USER root
+RUN docker-php-serversideup-set-id www-data $USER_ID:$GROUP_ID  && \
+    docker-php-serversideup-set-file-permissions --owner $USER_ID:$GROUP_ID --service nginx
+USER www-data
+
+############################################
+# Production Image
+############################################
 FROM base as deploy
 COPY --chown=www-data:www-data . /var/www/html
+USER www-data

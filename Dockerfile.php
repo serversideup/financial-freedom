e1a785bbee6449d59b7abd4da79b88d0e8ca29b3
Jay Rogers <jay@521dimensions.com>
2024-05-16 14:41:51 -0500

    Upgrade Spin and to serversideup/php v3

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,15 +1,51 @@
+############################################
+# Base Image
+############################################
+
 # Learn more about the Server Side Up PHP Docker Images at:
 # https://serversideup.net/open-source/docker-php/
+FROM serversideup/php:8.3-fpm-nginx-alpine as base
 
-FROM serversideup/php:beta-8.3-unit as base
+## Uncomment if you need to install additional PHP extensions
+# USER root
+# RUN install-php-extensions bcmath gd
 
+############################################
+# Development Image
+############################################
 FROM base as development
 
-# Fix permission issues in development by setting the "www-data"
-# user to the same user and group that is running docker.
+# We can pass USER_ID and GROUP_ID as build arguments
+# to ensure the www-data user has the same UID and GID
+# as the user running Docker.
 ARG USER_ID
 ARG GROUP_ID
-RUN docker-php-serversideup-set-id www-data ${USER_ID} ${GROUP_ID}
 
+# Switch to root so we can set the user ID and group ID
+USER root
+
+# Set the user ID and group ID for www-data
+RUN docker-php-serversideup-set-id www-data $USER_ID:$GROUP_ID  && \
+    docker-php-serversideup-set-file-permissions --owner $USER_ID:$GROUP_ID --service nginx
+
+# Drop privileges back to www-data    
+USER www-data
+
+############################################
+# CI image
+############################################
+FROM base as ci
+
+# Sometimes CI images need to run as root
+# so we set the ROOT user and configure
+# the PHP-FPM pool to run as www-data
+USER root
+RUN echo "user = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf && \
+    echo "group = www-data" >> /usr/local/etc/php-fpm.d/docker-php-serversideup-pool.conf
+
+############################################
+# Production Image
+############################################
 FROM base as deploy
 COPY --chown=www-data:www-data . /var/www/html
+USER www-data

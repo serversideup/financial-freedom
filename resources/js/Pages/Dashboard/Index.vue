dad4cc641bf2077db42d5949ce4e3270952c36de
Dan Pastori <dan@521dimensions.com>
2024-09-13 15:59:55 -0500

    WIP

--- resources/js/Pages/Dashboard/Index.vue
+++ resources/js/Pages/Dashboard/Index.vue
@@ -2,7 +2,7 @@
     <Head :title="'Dashboard'"/>
 
     <div class="w-full flex flex-col overflow-y-scroll">
-        <div class="flex w-full justify-between mb-6">
+        <div class="flex w-full justify-between mb-8">
             <h1 class="font-semibold font-sans text-[#F5F5F6] text-3xl">Financial Performance Summary</h1>
             <div class="flex items-center space-x-3">
                 <button class="py-[10px] px-[14px] rounded-lg border border-[#333741] bg-[#161B26] font-sans text-[#CECFD2] font-semibold flex items-center">
@@ -22,7 +22,24 @@
         </div>
 
         <div class="grid grid-cols-12 gap-x-6 gap-y-8">
-            
+            <SpendingBreakdown/>
+
+            <SpendingComparison/>
+
+            <PlannedIncomeExpense/>
+
+            <div class="col-span-6 p-6 border border-[#1F242F] rounded-xl">
+                <div class="flex items-center justify-between">
+                    <h3 class="text-[#F5F5F6] font-sans font-semibold">Cash Flow</h3>
+                    <button class="w-5 h-5 flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                            <path d="M9.99984 10.8334C10.4601 10.8334 10.8332 10.4603 10.8332 10C10.8332 9.5398 10.4601 9.16671 9.99984 9.16671C9.5396 9.16671 9.1665 9.5398 9.1665 10C9.1665 10.4603 9.5396 10.8334 9.99984 10.8334Z" stroke="#85888E" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M9.99984 5.00004C10.4601 5.00004 10.8332 4.62694 10.8332 4.16671C10.8332 3.70647 10.4601 3.33337 9.99984 3.33337C9.5396 3.33337 9.1665 3.70647 9.1665 4.16671C9.1665 4.62694 9.5396 5.00004 9.99984 5.00004Z" stroke="#85888E" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M9.99984 16.6667C10.4601 16.6667 10.8332 16.2936 10.8332 15.8334C10.8332 15.3731 10.4601 15 9.99984 15C9.5396 15 9.1665 15.3731 9.1665 15.8334C9.1665 16.2936 9.5396 16.6667 9.99984 16.6667Z" stroke="#85888E" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </div>
 
     </div>
@@ -37,6 +54,8 @@ export default {
 </script>
 
 <script setup>
-import SecondaryButton from '@/Components/SecondaryButton.vue';
+import PlannedIncomeExpense from './Partials/PlannedIncomeExpense.vue';
+import SpendingBreakdown from './Partials/SpendingBreakdown.vue';
+import SpendingComparison from './Partials/SpendingComparison.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
---
+++ resources/js/Pages/Dashboard/Partials/SpendingComparison.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="col-span-8 p-6 border border-[#1F242F] rounded-xl">
+        <div class="flex items-center justify-between">
+            <div class="flex flex-col">
+                <h3 class="text-[#F5F5F6] font-sans font-semibold">Spending Comparison</h3>
+                <span class="mt-1 text-[#94969C] font-sans text-sm">Visualize your spending habits.</span>
+            </div>
+
+            <select class="bg-[#161B26] rounded-md border border-[#333741] font-semibold text-sm text-[#CECFD2] py-[10px] pl-[14px] pr-9">
+                <option>This Month Vs. Last Month</option>
+                <option>This Year Vs. Last Year</option>
+            </select>
+        </div>
+    </div>
+</template>

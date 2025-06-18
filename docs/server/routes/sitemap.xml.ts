62a5e191cd66140b9e22fbcf6fbd6a9c2525ef05
Jay Rogers <jaydrogers@users.noreply.serversideup.net>
2025-06-18 13:03:37 -0500

    Fix sitemap

--- docs/server/routes/sitemap.xml.ts
+++ docs/server/routes/sitemap.xml.ts
@@ -4,12 +4,12 @@ export default defineEventHandler(async (event) => {
     // Fetch all documents
     const docs = await serverQueryContent(event).find()
     const sitemap = new SitemapStream({
-        hostname: 'https://financialfreedom.app'
+        hostname: 'https://serversideup.net'
     })    
 
     for (const doc of docs) {
         sitemap.write({
-            url: doc._path,
+            url: '/open-source/financial-freedom'+doc._path,
             changefreq: 'monthly'
         })
     }
